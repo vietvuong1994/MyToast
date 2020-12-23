51489544a891595862b53038fee98b7885dba95d
vietvuong1994 <vietvuong1994@gmail.com>
2020-12-23 08:54:35 +0700

    fix toast

--- mytoast/src/main/java/com/example/mytoast/MyToast.java
+++ mytoast/src/main/java/com/example/mytoast/MyToast.java
@@ -1,22 +1,14 @@
 package com.example.mytoast;
 
-import android.app.Application;
 import android.content.Context;
 import android.widget.Toast;
 
-public class MyToast extends Application {
+public class MyToast  {
 
-    private static Context context;
-
-    public MyToast() {
-        context = getApplicationContext();
-    }
-
-    public static void showToast(String message){
+    public static void showToast(Context context, String message){
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, message, duration);
         toast.show();
     }
-
 }
